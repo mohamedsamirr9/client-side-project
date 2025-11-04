014018a905b7044e62f1764c16e4fce1e6e7814d
AhmedRady130 <arady6903@gmail.com>
2025-11-04 15:45:14 +0200

    add feature for home

--- register.js
+++ register.js
@@ -1,45 +1,10 @@
-// console.log("hi");
-// const loginBtn = document.querySelector("#login");
-// const registerBtn = document.querySelector("#register");
-// const loginForm = document.querySelector(".login-form");
-// console.log(loginForm);
-
-// const registerForm = document.querySelector(".register-form");
-// loginBtn.addEventListener("click", () => {
-//   loginBtn.style.backgroundColor = "#21264D";
-
-//   registerBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
-
-//   loginForm.style.left = "50%";
-
-//   registerForm.style.left = "-50%";
-
-//   loginForm.style.opacity = 1;
-
-//   registerForm.style.opacity = 0;
-// });
-// registerBtn.addEventListener("click", () => {
-//   loginBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
-
-//   registerBtn.style.backgroundColor = "#21264D";
-
-//   loginForm.style.left = "150%";
-
-//   registerForm.style.left = "50%";
-
-//   loginForm.style.opacity = 0;
-
-//   registerForm.style.opacity = 1;
-// });
-/////////////////////////
-// === switch between login/register ===
 const loginBtn = document.querySelector("#login");
 const registerBtn = document.querySelector("#register");
 const loginForm = document.querySelector(".login-form");
 const registerForm = document.querySelector(".register-form");
 
 loginBtn.addEventListener("click", () => {
-  loginBtn.style.backgroundColor = "#21264D";
+  loginBtn.style.backgroundColor = "#855706";
   registerBtn.style.backgroundColor = "rgba(255,255,255,0.2)";
   loginForm.style.left = "50%";
   registerForm.style.left = "-50%";
@@ -48,7 +13,7 @@ loginBtn.addEventListener("click", () => {
 });
 
 registerBtn.addEventListener("click", () => {
-  registerBtn.style.backgroundColor = "#21264D";
+  registerBtn.style.backgroundColor = "#855706";
   loginBtn.style.backgroundColor = "rgba(255,255,255,0.2)";
   loginForm.style.left = "150%";
   registerForm.style.left = "50%";
@@ -58,19 +23,27 @@ registerBtn.addEventListener("click", () => {
 
 // === Validation + Local Storage ===
 
-const registerEmail = document.querySelector('.register-form input[placeholder="Email"]');
-const registerUsername = document.querySelector('.register-form input[placeholder="User Name"]');
-const registerPassword = document.querySelector('.register-form input[placeholder="Password"]');
-const registerSubmit = document.querySelector('.register-form .submit-btn');
+const registerEmail = document.querySelector(
+  '.register-form input[placeholder="Email"]'
+);
+const registerUsername = document.querySelector(
+  '.register-form input[placeholder="User Name"]'
+);
+const registerPassword = document.querySelector(
+  '.register-form input[placeholder="Password"]'
+);
+const registerSubmit = document.querySelector(".register-form .submit-btn");
 const createsucess = document.querySelector(".create-sucess");
 
-
-
 // login inputs
-const loginUsername = document.querySelector('.login-form input[placeholder="User Name"]');
-const loginPassword = document.querySelector('.login-form input[placeholder="Password"]');
-const loginSubmit = document.querySelector('.login-form .submit-btn');
-const error=document.querySelector('.error');
+const loginUsername = document.querySelector(
+  '.login-form input[placeholder="User Name"]'
+);
+const loginPassword = document.querySelector(
+  '.login-form input[placeholder="Password"]'
+);
+const loginSubmit = document.querySelector(".login-form .submit-btn");
+const error = document.querySelector(".error");
 
 // save users ion local storge
 function saveUser(user) {
@@ -81,7 +54,7 @@ function saveUser(user) {
 // validation
 registerSubmit.addEventListener("click", (e) => {
   e.preventDefault();
-createsucess.style.display="block";
+  createsucess.style.display = "block";
   const email = registerEmail.value.trim();
   const username = registerUsername.value.trim();
   const password = registerPassword.value.trim();
@@ -92,17 +65,16 @@ createsucess.style.display="block";
   }
 
   let users = JSON.parse(localStorage.getItem("users")) || [];
-  if (users.find(u => u.username === username)) {
+  if (users.find((u) => u.username === username)) {
     alert("Username already exists");
     return;
   }
 
   saveUser({ email, username, password });
-  
+
   registerEmail.value = "";
   registerUsername.value = "";
   registerPassword.value = "";
-
 });
 
 // login
@@ -113,17 +85,18 @@ loginSubmit.addEventListener("click", (e) => {
   const password = loginPassword.value.trim();
 
   let users = JSON.parse(localStorage.getItem("users")) || [];
-  let validUser = users.find(u => u.username === username && u.password === password);
+  let validUser = users.find(
+    (u) => u.username === username && u.password === password
+  );
 
   if (validUser) {
     localStorage.setItem("currentUser", JSON.stringify(validUser));
-    var ref =window.location.href = "./question.html";
-  } else{
-    error.style.display="block";
+    var ref = (window.location.href = "./question.html");
+  } else {
+    error.style.display = "block";
   }
 });
 
 window.addEventListener("DOMContentLoaded", () => {
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
- 
 });
