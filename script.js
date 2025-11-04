18c9c49e11d0c54f1f9a43fb9f3f6feaabd688f9
soadwalid <soadwalid964@gmail.com>
2025-11-04 16:25:13 +0200

    add links

--- script.js
+++ script.js
@@ -180,7 +180,7 @@ if (document.body.classList.contains("question-page")) {
     }
   }
 
-  var totalTime = 5;
+  var totalTime = 15;
   var timeDisplay = document.getElementById("time");
   var progressBar = document.querySelector(".progress");
 
